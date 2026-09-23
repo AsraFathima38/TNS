14b411f44ef264d0531b147a74b48324e6a68d6a
AsraFathima38 <asrafathima38@gmail.com>
2026-09-23 19:17:16 +0530

    Initial commit

---
+++ c2tc_core_java_vtu-master/VTUProject/src/com/tnsif/vtu/basics/FirstProgram.java
@@ -0,0 +1,15 @@
+package com.tnsif.vtu.basics;
+/**
+ * 
+ */
+
+/**
+ * @author Sagar
+ *
+ */
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Thread
+	}
+}
